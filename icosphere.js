45a6ba28c2639afcaf315f828f6e4fcee2b6e3f1
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-05-25 16:46:56 +0200

    cleanup indentation

--- icosphere.js
+++ icosphere.js
@@ -19,9 +19,9 @@ class Icosphere {
     // Initialize triangles.
     this.triangles = [
       0, 1, 4, 0, 4, 9, 9, 4, 5, 4, 8, 5, 4, 1, 8,
-  		8, 1, 10, 8, 10, 3, 5, 8, 3, 5, 3, 2, 2, 3, 7,
-  		7, 3, 10, 7, 10, 6, 7, 6, 11, 11, 6, 0, 0, 6, 1,
-  		6, 10, 1, 9, 11, 0, 9, 2, 11, 9, 5, 2, 7, 11, 2
+      8, 1, 10, 8, 10, 3, 5, 8, 3, 5, 3, 2, 2, 3, 7,
+      7, 3, 10, 7, 10, 6, 7, 6, 11, 11, 6, 0, 0, 6, 1,
+      6, 10, 1, 9, 11, 0, 9, 2, 11, 9, 5, 2, 7, 11, 2
     ]
   }
 
@@ -33,7 +33,7 @@ class Icosphere {
     const newVertices = [];
     const newTriangles = [];
     const normals = [];
-    
+
     for (let i = 0; i < this.triangles.length; i += 3) {
       const t1 = this.triangles[i];
       const t2 = this.triangles[i + 1];
